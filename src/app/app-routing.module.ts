92b131d0c167bc6f3911ac730736202c40e8a825
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-05-19 20:50:41 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+
+const routes: Routes = [
+  {
+    path : '',
+    component : ComingSoonComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
